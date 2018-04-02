5bb749f96709ddbe62bf1baf27e73dee6e0f4396
Dean Corn <cornisath@gmail.com>
2018-04-02 14:03:54 -0400

    4/2 classwork

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ var express = require('express')
 var app = express()
 
 const servername = "localhost";
-const port = 8080;
+const port = 9080;
 
 const simple = require('./simpleController');
+const game = require('./game/controller');
 
 
-app.use('/simple', simple).listen(port);
+app
+    .use('/simple', simple)
+    .use('/game', game)
+    .listen(port);
+
 
 console.log("running on http://" + servername + ":" + port)
